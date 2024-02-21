4af59cabd6812742eedf86e4ae2775a32ff462e2
kola kolade <kolakolade@gmail.com>
2024-02-22 00:06:16 +0100

    Simple express app

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3200;
+
+// 404 page
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Page not found');
+});
+
+// 505 page
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.type('text/plain');
+  res.status(505);
+  res.send('505 - Server error');
+});
+
+app.listen(port, () =>
+  console.log(
+    `Express started on http://localhost:${port}; ` +
+      `press Ctrl-C to terminate.`
+  )
+);
