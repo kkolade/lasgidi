728f50c6fd7514b426652b0785558cff1e328273
kola kolade <kolakolade@gmail.com>
2024-05-23 21:09:54 +0100

    Set up views and styling

--- server.js
+++ server.js
@@ -1,45 +1,61 @@
+// Node core modules
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// Express
 import express from 'express';
 
+// Dependencies modules
 import 'dotenv/config';
 import ejs from 'ejs';
+import expressLayouts from 'express-ejs-layouts';
 
+// Variables
 const port = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
+// Static Files
+app.use(express.static('public'));
 
+// Setup view engine
+app.use(expressLayouts);
+app.set('layout', path.join('./layouts/main'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+const user = {
+  firstName: 'Leke',
+  lastName: 'Kolade',
+};
 // Home page
 app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Welcome to Lasgidi');
+  res.render('pages/index', {
+    title: 'Homepage',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About Lasgidi');
+  res.render('pages/about', {
+    title: 'About Page',
+    layout: './layouts/sidebar',
+  });
 });
 
 // 404 - Page not found
 app.use((req, res) => {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Page not found!');
+  res.render('pages/404');
 });
 
 // 500 - Server Error
 app.use((err, req, res, next) => {
   console.error(err.message);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Internal Server Error!');
+  res.render('pages.500');
 });
 
 app.listen(port, () => {
