02a89c9127a1868f4d09e84a770b69e6f69e4147
kola kolade <kolakolade@gmail.com>
2024-05-23 21:56:43 +0100

    Finish setup for views and templating

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // Node core modules
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
 // Express
 import express from 'express';
 
 // Dependencies modules
 import 'dotenv/config';
-import ejs from 'ejs';
+// import ejs from 'ejs';
 import expressLayouts from 'express-ejs-layouts';
 
 // Variables
 const port = process.env.PORT;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -22,9 +22,9 @@ app.use(express.static('public'));
 
 // Setup view engine
 app.use(expressLayouts);
-app.set('layout', path.join('./layouts/main'));
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', ejs.renderFile);
+app.set('layout', './layouts/main');
+app.set('views', 'views');
+// app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 
 const user = {
@@ -48,14 +48,19 @@ app.get('/about', (req, res) => {
 // 404 - Page not found
 app.use((req, res) => {
   res.status(404);
-  res.render('pages/404');
+  res.render('pages/404', {
+    title: 'Page Not Found',
+  });
 });
 
 // 500 - Server Error
 app.use((err, req, res, next) => {
   console.error(err.message);
   res.status(500);
-  res.render('pages.500');
+  res.render('pages.500'),
+    {
+      title: 'Server Error',
+    };
 });
 
 app.listen(port, () => {
